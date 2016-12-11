7cba725d4c0af8188737578aa2dda9ace1e86714
Francis Y. Yan <francisyyan@stanford.edu>
2016-12-10 23:03:42 -0800

    on_off_interval.py: get interval times between ON and OFF states

---
+++ on_off_interval.py
@@ -0,0 +1,30 @@
+import json
+
+
+def get_on_off_interval():
+    # read run times
+    with open('pantheon_metadata.json') as metadata_file:
+        metadata_dict = json.load(metadata_file)
+    run_times = metadata_dict['run_times']
+
+    # get on off interval
+    on_off_interval = []
+
+    for run_id in xrange(1, 1 + run_times):
+        log = open('default_tcp_datalink_run%s.log' % run_id)
+
+        last_ts = None
+        for line in log:
+            items = line.split()
+            if len(items) < 4 or items[1] != '-':
+                continue
+
+            ts = int(items[0])
+            if last_ts and ts != last_ts:
+                on_off_interval.append(ts - last_ts)
+
+            last_ts = ts
+
+        log.close()
+
+    return on_off_interval
