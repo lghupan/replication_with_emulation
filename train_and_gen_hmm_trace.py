8819a04594b640e3677c2272b64ac4d63bafa196
Francis Y. Yan <francisyyan@stanford.edu>
2016-12-11 01:47:30 -0800

    train_and_gen_hmm_trace.py: bug fix in sample matrix

--- train_and_gen_hmm_trace.py
+++ train_and_gen_hmm_trace.py
@@ -1,56 +1,40 @@
 #!/usr/bin/env python
 
-import json
 import numpy as np
 from hmmlearn import hmm
 
 
 # get arrivals or silence per ms
 def get_arrivals_per_ms():
-    # read run times
-    with open('pantheon_metadata.json') as metadata_file:
-        metadata_dict = json.load(metadata_file)
-    run_times = metadata_dict['run_times']
-
     # get arrivals per ms
     arrivals_per_ms = []
     pkts = 0
-    last_len = 0
-    lengths = []
 
-    for run_id in xrange(1, 1 + run_times):
-        log = open('default_tcp_datalink_run%s.log' % run_id)
+    log = open('/usr/share/mahimahi/traces/Verizon-LTE-driving.down')
 
-        last_ts = None
-        for line in log:
-            items = line.split()
-            if len(items) < 4 or items[1] != '-':
-                continue
+    last_ts = None
+    for line in log:
+        ts = int(line)
+        if ts != last_ts:
+            if last_ts != None:
+                arrivals_per_ms.append(pkts)
 
-            ts = int(items[0])
-            if ts != last_ts:
-                if last_ts != None:
-                    arrivals_per_ms.append(pkts)
+                for silent_ts in xrange(last_ts + 1, ts):
+                    arrivals_per_ms.append(0)
 
-                    for silent_ts in xrange(last_ts + 1, ts):
-                        arrivals_per_ms.append(0)
+            pkts = 1
+        else:
+            pkts += 1
 
-                pkts = 1
-            else:
-                pkts += 1
+        last_ts = ts
 
-            last_ts = ts
+    log.close()
 
-        log.close()
-
-        lengths.append(len(arrivals_per_ms) - last_len)
-        last_len = len(arrivals_per_ms)
-
-    return arrivals_per_ms, lengths
+    return arrivals_per_ms
 
 
 def train_hmm():
-    X, lengths = get_arrivals_per_ms()
+    X = get_arrivals_per_ms()
 
     # only allow output to be [0, 20]
     for i in xrange(len(X)):
@@ -78,7 +62,7 @@ def train_hmm():
     model.emissionprob_ = emission_prob
 
     X = np.array([X])
-    model.fit(X.T, lengths)
+    model.fit(X.T)
 
     return model
 
@@ -89,7 +73,7 @@ def generate_trace(model):
         trace = open('hmm_trace_%s' % run_id, 'w')
 
         for ts in xrange(X.size):
-            for i in xrange(X[ts]):
+            for i in xrange(X[ts][0]):
                 trace.write('%s\n' % ts)
 
         trace.close()
